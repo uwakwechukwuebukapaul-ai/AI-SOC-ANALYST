7a6565e58eb49c69764249bb838065ed3b538384
Uwakwe Chukwuerika Paul <uwakwechukwuebukapaul@gmail.com>
2026-07-29 08:32:15 +0100

    Added SOC dashboard authentication

--- web_dashboard.py
+++ web_dashboard.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 from soc_pipeline import run_soc_pipeline
+
+from auth import login
 
 
 app = Flask(__name__)
 
+app.secret_key = "AI_SOC_SECRET_KEY"
+
+
+
+@app.route("/login", methods=["GET","POST"])
+def login_page():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+
+        password = request.form["password"]
+
+
+        if login(username, password):
+
+            session["analyst"] = username
+
+            return redirect("/")
+
+
+    return render_template("login.html")
+
+
+
 
 @app.route("/")
 def home():
+
+    if "analyst" not in session:
+
+        return redirect("/login")
+
 
     alerts = run_soc_pipeline()
 
@@ -16,6 +48,17 @@ def home():
         "index.html",
         alerts=alerts
     )
+
+
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/login")
+
 
 
 
